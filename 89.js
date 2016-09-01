8719601e4e20a2fd78787257c7db2ecd364eadef
quangmen93 <quangmen93@gmail.com>
2016-09-01 10:54:30 +0700

    Update 89.js

--- 89.js
+++ 89.js
@@ -1,7 +1,13 @@
 var password;
-var pass1="pass1";
-var pass2="pass2";
-
+var pass1="Kirara Asuka";
+var pass2="Akiho Yoshizawa";
+var pass3="Ami Ishihara";
+var pass4="Eri Hosaka";
+var pass5="Hara Chihiro (Leila Aizaki)";
+var pass6="Jun Aizawa";
+var pass7="Tsuna Kimura";
+var pass8="Yui Nishikawa";
+var pass9="Yumi Maeda";
 password=prompt('Please enter your password to view this page!','');
 if (password==pass1 || password==pass2)
 alert('Password Correct! Click OK to enter!');
