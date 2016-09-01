85c62f8254390f7ba963aba591f5b6a0a3761bab
quangmen93 <quangmen93@gmail.com>
2016-09-01 11:34:42 +0700

    Add files via upload

---
+++ 87.js
@@ -0,0 +1,18 @@
+var password;
+var pass1="Kirara Asuka";
+var pass2="Akiho Yoshizawa";
+var pass3="Ami Ishihara";
+var pass4="Eri Hosaka";
+var pass5="Hara Chihiro (Leila Aizaki)";
+var pass6="Jun Aizawa";
+var pass7="Tsuna Kimura";
+var pass8="Yui Nishikawa";
+var pass9="Yumi Maeda";
+var pass10="1";
+password=prompt('Please enter your password to view this page!','');
+if (password==pass1 || password==pass2 || password==pass3 || password==pass4 || password==pass5 || password==pass6 || password==pass7 || password==pass8 || password==pass9 || password==pass10)
+alert('Password Correct! Click OK to enter!');
+else
+{
+window.location="https://sexjavhay.blogspot.com/";
+}
