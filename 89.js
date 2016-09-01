2461be800431a70ef89809c250f0443e9425dee1
quangmen93 <quangmen93@gmail.com>
2016-09-01 10:49:49 +0700

    Add files via upload

---
+++ 89.js
@@ -0,0 +1,11 @@
+var password;
+var pass1="pass1";
+var pass2="pass2";
+
+password=prompt('Please enter your password to view this page!','');
+if (password==pass1 || password==pass2)
+alert('Password Correct! Click OK to enter!');
+else
+{
+window.location="http://hienthieugia.blogspot.com/";
+}
