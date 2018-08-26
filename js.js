2256f5c9bf7abf43faa2c5ff62d784dc81dac816
quangmen93 <quangmen93@gmail.com>
2018-08-27 02:27:22 +0700

    Add files via upload

---
+++ js.js
@@ -0,0 +1,10 @@
+jQuery(document).ready(function() {                    jQuery('#oscitas-restabs-1-down-load-40845').tabdrop({'text': '<i class="res_tab_icon"></i>More'});            var tabHashId = window.location.hash.substr(1);
+            if (tabHashId) {
+                jQuery('#oscitas-restabs-1-down-load-40845 a[href="#'+tabHashId+'"]').tab('show');
+            }                    jQuery('#oscitas-restabs-1-down-load-38856').tabdrop({'text': '<i class="res_tab_icon"></i>More'});            var tabHashId = window.location.hash.substr(1);
+            if (tabHashId) {
+                jQuery('#oscitas-restabs-1-down-load-38856 a[href="#'+tabHashId+'"]').tab('show');
+            }                    jQuery('#oscitas-restabs-1--99112').tabdrop({'text': '<i class="res_tab_icon"></i>More'});            var tabHashId = window.location.hash.substr(1);
+            if (tabHashId) {
+                jQuery('#oscitas-restabs-1--99112 a[href="#'+tabHashId+'"]').tab('show');
+            }});
